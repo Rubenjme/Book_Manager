64041b56e08eb31b326c8af26351b54cf521a44d
Rubenjme <rubjaicab@gmail.com>
2025-03-26 11:04:59 +0100

    Se añaden los botones de la parte derecha

--- front.py
+++ front.py
@@ -33,17 +33,37 @@ e4 = Entry(window, textvariable=isbn_text)
 e4.grid(row=1, column=3)
 
 
-# --- Parte superior ---
+# --- Caja de texto y Scrollbar ---
 list1=Listbox(window, height=6, width=35)
 list1.grid(row=2, column=0, rowspan=6, columnspan=2)
 
 sb1=Scrollbar(window)
-sb1.grid(row=2, column=2, rowspan=6)
+sb1.grid(row=2, column=2, rowspan=6) # Posición del scrollbar
 
 list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
 
 
-window.mainloop()
+# --- Botones ---
+b1=Button(window, text="View all", width=12)
+b1.grid(row=2, column=3)
 
+b2=Button(window, text="Search entry", width=12)
+b2.grid(row=3, column=3)
+
+b3=Button(window, text="Add entry", width=12)
+b3.grid(row=4, column=3)
+
+b4=Button(window, text="Update", width=12)
+b4.grid(row=5, column=3)
+
+b5=Button(window, text="Delete", width=12)
+b5.grid(row=6, column=3)
+
+b6=Button(window, text="Close", width=12)
+b6.grid(row=7, column=3)
+
+
+
+window.mainloop()
 
