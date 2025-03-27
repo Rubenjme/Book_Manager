caaaf657b1107325bfa18b76bbfd5e16567c9ad3
Rubenjme <rubjaicab@gmail.com>
2025-03-27 11:59:37 +0100

    Se añaden el resto de funciones de los botones

--- frontend.py
+++ frontend.py
@@ -20,10 +20,32 @@ def add_command():
     list1.delete(0, END)
     list1.insert(END, (title_text.get(), author_text.get(), year_text.get(), isbn_text.get()))
 
+def delete_command():
+    backend.delete(selected_tuple[0])
+    view_command()
+
+def update_command():
+    backend.update(selected_tuple[0], title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
+    view_command()
+
+def get_selected_row(event):
+    global selected_tuple
+    index = list1.curselection()
+    if index:
+        selected_tuple = list1.get(index)
+        e1.delete(0, END)
+        e1.insert(END, selected_tuple[1])
+        e2.delete(0, END)
+        e2.insert(END, selected_tuple[2])
+        e3.delete(0, END)
+        e3.insert(END, selected_tuple[3])
+        e4.delete(0, END)
+        e4.insert(END, selected_tuple[4])
+
 # --- Ventana ---
 window = Tk()
 
-# --- Parte superior ---
+# --- Etiquetas de la parte superior ---
 l1 = Label(window, text="Title")
 l1.grid(row=0, column=0)
 
@@ -36,7 +58,7 @@ l3.grid(row=1, column=0)
 l4 = Label(window, text="ISBN")
 l4.grid(row=1, column=2)
 
-
+# --- Entradas ---
 title_text = StringVar()
 e1 = Entry(window, textvariable=title_text)
 e1.grid(row=0, column=1)
@@ -75,16 +97,17 @@ b2.grid(row=3, column=3)
 b3=Button(window, text="Add entry", width=12, command=add_command)
 b3.grid(row=4, column=3)
 
-b4=Button(window, text="Update", width=12)
+b4=Button(window, text="Update", width=12, command=update_command)
 b4.grid(row=5, column=3)
 
-b5=Button(window, text="Delete", width=12)
+b5=Button(window, text="Delete", width=12, command=delete_command)
 b5.grid(row=6, column=3)
 
-b6=Button(window, text="Close", width=12)
+b6=Button(window, text="Close", width=12, command=window.destroy)
 b6.grid(row=7, column=3)
 
-
+# --- Vincular selección del Listbox ---
+list1.bind('<<ListboxSelect>>', get_selected_row)
 
 window.mainloop()
 
